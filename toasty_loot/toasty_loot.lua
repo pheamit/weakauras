eaf8bf5be3df413a447c4e1ee6e6f3624dbea71b
pheamit <miturtow@gmail.com>
2025-05-01 17:35:41 +0300

    Simplified the logic, dynamic item count, no calculation errors should be possible anymore.

--- toasty_loot/toasty_loot.lua
+++ toasty_loot/toasty_loot.lua
@@ -1,3 +1,4 @@
+local aura_env -- getting rid of the "undefined global" warning
 -- Money
 -- 133789 Copper, 133787 Silver, 133785 Gold
 -- 133789 Copper, 133787 Silver, 133785 Gold
@@ -40,7 +41,9 @@ function(allstates, event, ...)
     if event == "CHAT_MSG_LOOT" and select(5, ...) == UnitName("player") then
         local text = select(1, ...)
         local link = text:match("(|c.+|r)")
-        local name, _, quality, _, _, _, _, _, _, icon = C_Item.GetItemInfo(link)
+        local itemID, itemType, itemSubType, itemEquipLoc, icon, classID, subClassID = C_Item.GetItemInfoInstant(link)
+        local quality = C_Item.GetItemQualityByID(link)
+        local name = C_Item.GetItemNameByID(link)
         local r, g, b, hex = C_Item.GetItemQualityColor(quality)
         local itemsLooted = ""
         -- % is an escape character for search pattern special characters such as '.'
@@ -49,33 +52,38 @@ function(allstates, event, ...)
         else
             itemsLooted = text:sub(text:find("rx") + 2, text:find("%.") - 1)
         end
-        -- This guid is needed for the WA table to be unique and not override itself
-        local guid = select(11, ...)
-        local text = "|c" .. hex .. name .. "|cff00ff00 x " .. itemsLooted
-        aura_env.item = {
-            guid = guid,
-            text = text,
-            link = link,
-            icon = icon,
-            looted = itemsLooted,
-            count = "",
-        }
-    end 
-    if event == "BAG_UPDATE_DELAYED" and aura_env.item then
-        local count = C_Item.GetItemCount(aura_env.item.link)
-        local totalCount = count > 0 and count or aura_env.item.looted
-        aura_env.item.count = aura_env.config["count"] and totalCount or ""
-        allstates[aura_env.item.guid] = {
-            show = true,
-            changed = true,
-            progressType = "timed",
-            duration = aura_env.config.duration,
-            icon = aura_env.item.icon,
-            autoHide = true,
-            link = aura_env.item.link,
-        }
+        if allstates[itemID] and allstates[itemID].show == false then
+            allstates[itemID].itemsLooted = allstates[itemID].itemsLooted + itemsLooted
+            allstates[itemID].timestamp = timestamp
+        else
+            allstates[itemID] = {
+                show = false,
+                changed = true,
+                progressType = "timed",
+                expirationTime = GetTime() + aura_env.config.duration,
+                duration = aura_env.config.duration,
+                autoHide = true,
+                icon = icon,
+                link = link,
+                hex = hex,
+                name = name,
+                lootText = "",
+                itemsLooted = itemsLooted,
+            }
+        end
+    end
+    if event == "BAG_UPDATE_DELAYED" then
+        for itemID, state in pairs(allstates) do
+            if aura_env.config["count"] then
+                local initialCount = C_Item.GetItemCount(itemID)
+                -- Check for when C_Item.GetItemCount() returns 0 if item is new for the inventory
+                local totalCount = initialCount > 0 and initialCount or state.itemsLooted
+                state.itemCount = totalCount
+            end
+            state.lootText = "|c" .. state.hex .. state.name .. "|cff00ff00 x " .. state.itemsLooted
+            state.show = true
+            state.changed = true
+        end
         return true
     end
 end
-
-
